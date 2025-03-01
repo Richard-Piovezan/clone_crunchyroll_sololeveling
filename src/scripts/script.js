8369fa6915a3e9ab83c47ad0269650979bdc52e5
richard-piovezan <contato.piovezan@gmail.com>
2025-03-01 16:13:23 -0300

    create hero section // create stylesheet _hero.scss // create function to change season with html select // add new images in ./src/images/ep/t1 and .src/images/ep/t2

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,34 +1,10 @@
-// function moreDetails() {
-//     const opacityOne = document.getElementById("hero-opacity-one");
-//     const opacityTwo = document.getElementById("hero-opacity-two");
-//     const allContent = document.getElementById("hero-all-content");
-//     const btnMoreDetails = document.getElementById("more-details--button");
-//     const allContentInfos = document.getElementById("hero-all-content-infos");
-
-//     if(allContent.style.display === "flex") {
-//         allContent.style.display = "none";
-//         allContentInfos.display = "none";
-//         opacityOne.style.color = "#A0A0A0";
-//         opacityTwo.style.color = "#59595B";
-//         btnMoreDetails.innerHTML = "mais detalhes";
-//         btnMoreDetails.style.marginTop = "0";
-//     } else {
-//         allContent.style.display = "flex";
-//         allContentInfos.opacity = "1";
-//         opacityOne.style.color = "#FFFFFF";
-//         opacityTwo.style.color = "#FFFFFF";
-//         btnMoreDetails.innerHTML = "menos detalhes";
-//         btnMoreDetails.style.marginTop = "10px";
-//     }
-// }
-
 function moreDetails() {
     const allContentStory = document.getElementById("hero-all-content-story");
     const allContentInfos = document.getElementById("hero-all-content-infos");
     const opacityOne = document.getElementById("hero-opacity-one");
     const opacityTwo = document.getElementById("hero-opacity-two");
     const btnDetails = document.getElementById("more-details--button");
-
+    
     if (allContentStory.style.display === "flex") {
         allContentStory.style.display = "none";
         allContentInfos.style.display = "none";
@@ -45,3 +21,30 @@ function moreDetails() {
         btnDetails.style.marginTop = "10px";
     }
 }
+
+function changeSeason() {
+    const selectSeasons = document.getElementById("select-season");
+    const showSeason1 = document.getElementById("season1");
+    const showSeason2 = document.getElementById("season2");
+
+    
+    selectSeasons.addEventListener("change", atualizarTemporada);
+    atualizarTemporada();
+
+
+    function atualizarTemporada() {
+        if (selectSeasons.value === "T1: Solo Leveling") {
+            console.log("T1");
+            showSeason1.classList.add("shows__list--is-active");
+            showSeason2.classList.remove("shows__list--is-active");
+        } else if (selectSeasons.value === "T2: Solo Leveling") {
+            console.log("T2");
+            showSeason2.classList.add("shows__list--is-active");
+            showSeason1.classList.remove("shows__list--is-active");
+        }
+    }
+}
+
+window.onload = function () {
+    changeSeason();
+};
