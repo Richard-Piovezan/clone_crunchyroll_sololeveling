08bb839388d278c80e60e45c33fa6ffddf7553f6
richard-piovezan <contato.piovezan@gmail.com>
2025-03-03 18:58:14 -0300

    compressed js and style // change version sass to @5.1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 function styles() {
     return gulp
-        .src("./src/styles/*.scss")
-        .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
+        .src("./src/styles/style.scss")
+        .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
         .pipe(gulp.dest("./dist/styles"));
 }
 
@@ -16,7 +17,15 @@ function images() {
         .pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(styles, images);
+function scripts() {
+    return gulp
+        .src("./src/scripts/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/scripts"))
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
+    gulp.watch("./src/scripts/*.js", gulp.parallel(scripts))
 }
